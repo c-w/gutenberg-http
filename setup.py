9c34afad3f0f23760f1e091143b2852ed0028497
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-04-28 23:34:46 -0700

    Add simple setuptools script

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+with open('requirements.txt') as fobj:
+    install_requires = [line.strip() for line in fobj]
+
+
+with open('README.rst') as fobj:
+    long_description = fobj.read()
+
+
+packages = find_packages(exclude=['tests*'])
+scripts = ['runserver']
+
+
+setup(
+    name='gutenberg_http',
+    version='0.0.1',
+    author='Clemens Wolff',
+    author_email='clemens.wolff+pypi@gmail.com',
+    packages=packages,
+    url='https://github.com/c-w/gutenberg-http',
+    license='LICENSE.txt',
+    description='HTTP API for Project Gutenberg',
+    long_description=long_description,
+    include_package_data=True,
+    scripts=scripts,
+    install_requires=install_requires)
