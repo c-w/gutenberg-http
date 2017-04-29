0df6998a4d77d864d2e3909fef99e6b28118f4bd
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-04-28 21:36:49 -0700

    Just inline the little logic that there is

--- gutenberg_http/views.py
+++ gutenberg_http/views.py
@@ -1,24 +1,24 @@
+from gutenberg.query import get_etexts
+from gutenberg.query import get_metadata
 from sanic.request import Request
 from sanic.response import json
 
 from gutenberg_http import app
-from gutenberg_http.logic import find_texts
-from gutenberg_http.logic import lookup_metadata
 from gutenberg_http.parameters import parse_fields
 from gutenberg_http.parameters import parse_search
 
 
 @app.route('/texts/<text_id:int>')
 async def metadata(request: Request, text_id: int):
-    requested_fields = parse_fields(request.args.get('fields'))
-    field_values = lookup_metadata(text_id, requested_fields)
+    fields = parse_fields(request.args.get('fields'))
+    field_values = {field: get_metadata(field, text_id) for field in fields}
 
     return json({'text_id': text_id, 'metadata': field_values})
 
 
 @app.route('/search')
 async def search(request: Request):
-    requested_search = parse_search(request.args.get('q'))
-    search_results = find_texts(requested_search)
+    field, value = parse_search(request.args.get('q'))
+    search_results = get_etexts(field, value)
 
     return json({'text_ids': search_results})
