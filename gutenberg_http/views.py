b51f12ba79777b72c4d544e97a3b3d6657a32a4b
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-04-28 21:47:26 -0700

    Add endpoint to fetch body of gutenberg text

--- gutenberg_http/views.py
+++ gutenberg_http/views.py
@@ -1,7 +1,9 @@
+from gutenberg.acquire import load_etext
 from gutenberg.query import get_etexts
 from gutenberg.query import get_metadata
 from sanic.request import Request
 from sanic.response import json
+from sanic.response import text
 
 from gutenberg_http import app
 from gutenberg_http.parameters import parse_fields
@@ -11,9 +13,17 @@ from gutenberg_http.parameters import parse_search
 @app.route('/texts/<text_id:int>')
 async def metadata(request: Request, text_id: int):
     fields = parse_fields(request.args.get('fields'))
-    field_values = {field: get_metadata(field, text_id) for field in fields}
+    metadata_values = {field: get_metadata(field, text_id) for field in fields}
 
-    return json({'text_id': text_id, 'metadata': field_values})
+    return json({'text_id': text_id, 'metadata': metadata_values})
+
+
+# noinspection PyUnusedLocal
+@app.route('/texts/<text_id:int>/body')
+async def body(request: Request, text_id: int):
+    body_value = load_etext(text_id)
+
+    return text(body_value)
 
 
 @app.route('/search')
