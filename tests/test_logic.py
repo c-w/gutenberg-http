40e80253c9c53430f256db52d9c3013d420e3a87
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-04-29 00:16:05 -0700

    Add test for conjunctive query logic

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -38,3 +38,21 @@ class SearchTests(TestCase):
 
         logic.search('title eq The Jungle Book')
         self.assertEqual(mock_get_etexts.call_count, 2)
+
+    @patch.object(logic, 'get_etexts')
+    def test_conjunctive_query(self, mock_get_etexts):
+        self.given_texts(mock_get_etexts,
+                         ('language', 'en', {1, 2, 3}),
+                         ('author', 'Kipling, Rudyard', {2, 3, 4}))
+
+        result = logic.search('language eq en and author eq Kipling, Rudyard')
+
+        self.assertEqual(result, {'text_ids': {2, 3}})
+
+    @classmethod
+    def given_texts(cls, mock_get_etexts, *key_values):
+        def side_effect(key, value):
+            return next((result for k, v, result in key_values
+                         if k == key and v == value), None)
+
+        mock_get_etexts.side_effect = side_effect
