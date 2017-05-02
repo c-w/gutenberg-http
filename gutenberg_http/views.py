3d6c82712b234e030b133890c49d7f4f4d0c87aa
Clemens Wolff <clemens.wolff+git@gmail.com>
2017-05-02 00:37:39 -0700

    Don't block on views

--- gutenberg_http/views.py
+++ gutenberg_http/views.py
@@ -10,49 +10,49 @@ from gutenberg_http.logic import search as _search
 
 
 @app.route('/texts/<text_id:int>')
-def metadata(request: Request, text_id: int):
+async def metadata(request: Request, text_id: int):
     include = _metadata(text_id, request.args.get('include'))
     return json({'text_id': text_id, 'metadata': include})
 
 
 # noinspection PyUnusedLocal
 @app.route('/texts/<text_id:int>/body')
-def body(request: Request, text_id: int):
+async def body(request: Request, text_id: int):
     fulltext = _body(text_id)
     return json({'text_id': text_id, 'body': fulltext})
 
 
 # noinspection PyUnusedLocal
 @app.route('/search/<query>')
-def search(request: Request, query: str):
+async def search(request: Request, query: str):
     results = _search(query, request.args.get('include'))
     return json({'texts': results})
 
 
 # noinspection PyUnusedLocal
 @app.exception(InvalidUsage)
-def bad_request(request: Request, exception: InvalidUsage):
+async def bad_request(request: Request, exception: InvalidUsage):
     error = {'error': 'invalid_usage', 'message': exception.message}
     return json(error, exception.status_code)
 
 
 # noinspection PyUnusedLocal
 @app.exception(RequestTimeout)
-def timeout(request: Request, exception: RequestTimeout):
+async def timeout(request: Request, exception: RequestTimeout):
     error = {'error': 'timeout', 'message': 'The request timed out.'}
     return json(error, exception.status_code)
 
 
 # noinspection PyUnusedLocal
 @app.exception(Exception)
-def on_exception(request: Request, exception: Exception):
+async def on_exception(request: Request, exception: Exception):
     error = {'error': exception.__class__.__name__, 'message': str(exception)}
     return json(error, getattr(exception, 'status_code', 500))
 
 
 # noinspection PyUnusedLocal,PyProtectedMember
 @app.route('/healthcheck')
-def healthcheck(request: Request):
+async def healthcheck(request: Request):
     return json({
         'caches': {
             'metadata': _metadata.cache_info()._asdict(),
