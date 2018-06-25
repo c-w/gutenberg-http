80391d790cc061e97db1b0bc4c90d12fd920a42e
Clemens Wolff <clewolff@microsoft.com>
2018-06-25 18:04:56 -0400

    Add classifiers to setup.py
    Also:
    - Fix license field
    - Add python_requires

--- setup.py
+++ setup.py
@@ -21,9 +21,16 @@ setup(
     author_email='clemens.wolff+pypi@gmail.com',
     packages=packages,
     url='https://github.com/c-w/gutenberg-http',
-    license='LICENSE.txt',
+    license='Apache Software License',
     description='HTTP API for Project Gutenberg',
     long_description=long_description,
     include_package_data=True,
     scripts=scripts,
-    install_requires=install_requires)
+    install_requires=install_requires,
+    python_requires='>=3.5.*',
+    classifiers=[
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ])
