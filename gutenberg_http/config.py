2d1ed51158e163b01c1e24b02b1b34f9da691509
Clemens Wolff <clewolff@microsoft.com>
2019-04-23 12:33:28 -0400

    Update gh-pages root url

--- gutenberg_http/config.py
+++ gutenberg_http/config.py
@@ -5,4 +5,4 @@ BODY_CACHE_SIZE = int(getenv('GUTENBERG_HTTP_BODY_CACHE_SIZE', '64'))
 SEARCH_CACHE_SIZE = int(getenv('GUTENBERG_HTTP_SEARCH_CACHE_SIZE', '128'))
 
 TEST_PAGE_URL = getenv('GUTENBERG_HTTP_TEST_PAGE_URL',
-                       'https://c-w.github.io/gutenberg-http')
+                       'https://justamouse.com/gutenberg-http')
