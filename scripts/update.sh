60b4835a9b36aec895d09fc2345cce3940658715
Clemens Wolff <clewolff@microsoft.com>
2019-04-23 20:39:06 -0400

    Move deletion of old database into python

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,6 +9,4 @@ db="db$(date +%s)"
   cd "${basedir}"
 
   docker-compose exec web sh -c "GUTENBERG_DATA=/data/${db} python -m gutenberg_http initdb runserver"
-
-  find . -type d -name 'db*' -and -not -name "${db}" -and -not -path '*/.git/*' -exec rm -rf {} \;
 )
