f77ae3302989a8c17a2f926e1fe754fdcb0ce5ec
Clemens Wolff <clewolff@microsoft.com>
2019-04-23 23:47:09 -0400

    Ensure APP_DB gets persistet in dotenv

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -8,5 +8,7 @@ db="db$(date +%s)"
 (
   cd "${basedir}"
 
+  sed -i "s|^APP_DB=.*|APP_DB=${db}|g"
+
   docker-compose exec web sh -c "GUTENBERG_DATA=/data/${db} python -m gutenberg_http initdb runserver"
 )
