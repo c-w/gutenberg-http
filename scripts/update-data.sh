2d344f3f392a4c7aa536c2aad27e102ed260c868
Clemens Wolff <clewolff@microsoft.com>
2019-04-26 13:20:24 -0400

    Fix sed script

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -8,7 +8,7 @@ db="db$(date +%s)"
 (
   cd "${basedir}"
 
-  sed -i "s|^APP_DB=.*|APP_DB=${db}|g"
+  sed -i "s|^APP_DB=.*|APP_DB=${db}|g" .env
 
   docker-compose exec web sh -c "GUTENBERG_DATA=/data/${db} python -m gutenberg_http initdb runserver"
 )
