c39a54c74f2516a143df573577e02f1af8c70b4e
Clemens Wolff <clewolff@microsoft.com>
2019-04-26 12:03:03 -0400

    Add AppInsights instrumentation

--- gutenberg_http/__init__.py
+++ gutenberg_http/__init__.py
@@ -1,7 +1,9 @@
+from applicationinsights.flask.ext import AppInsights
 from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+AppInsights(app)
 
 import gutenberg_http.views  # noqa
