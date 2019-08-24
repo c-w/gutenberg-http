cf0ad2e98ed2a4aeb00772f7eea59ba6cae6c6dd
Clemens Wolff <clewolff@microsoft.com>
2019-08-24 19:40:40 -0400

    Add progress logging to database update script

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -10,5 +10,7 @@ db="db$(date +%s)"
 
   sed -i "s|^APP_DB=.*|APP_DB=${db}|g" .env
 
+  echo "[$(date)] Starting upgrade to ${db}"
   /usr/local/bin/docker-compose exec -T web sh -c "GUTENBERG_DATA=/data/${db} python -m gutenberg_http initdb runserver"
+  echo "[$(date)] Done with upgrade to ${db}"
 )
